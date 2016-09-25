6dbb23387fbf612cfda0f45deb532a36284b0efe
Reiner Jung <reiner.jung@email.uni-kiel.de>
2016-09-25 15:49:00 +0200

    added API baseline

---
+++ make-local-repo.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+BINDIR=$(cd "$(dirname "$0")"; pwd)
+
+LIBS=~/.p2/pool/plugins
+
+LIST=`cat << EOF
+de.uka.ipd.sdq.identifier
+org.palladiosimulator.pcm
+de.uka.ipd.sdq.units
+de.uka.ipd.sdq.stoex
+de.uka.ipd.sdq.probfunction
+protocom.extension
+org.eclipse.emf.cdo
+org.eclipse.emf.cdo.common
+org.eclipse.ocl
+EOF`
+
+MVNDIR="${BINDIR}/mvn-repo"
+
+if [ ! -d "${MVNDIR}" ] ; then
+	mkdir "${MVNDIR}"
+fi
+
+export GRADLE_DEPENDENCIES="${BINDIR}/gradle.dependencies"
+
+NEWLINE=$'\n'
+
+touch "${GRADLE_DEPENDENCIES}"
+
+for I in $LIST ; do
+	echo "Op $I"
+	for P in `find ${LIBS} -name "${I}_*.jar"` ; do
+		echo "$P"
+		if [ -f "$P" ] ; then
+			N=`basename $P`
+			ARTIFACT=`echo $N | sed 's/^\(.*\)_.*$/\1/'`
+			VERSION=`echo $N | sed 's/^.*_\(.*\)\.jar$/\1/'`
+			echo $N
+		#	echo $ARTIFACT
+		#	echo $VERSION
+			mvn org.apache.maven.plugins:maven-install-plugin:2.3.1:install-file -Dfile="$P" \
+				-DgroupId=extra -DartifactId=$ARTIFACT -Dpackaging=jar -Dversion=$VERSION \
+				-DlocalRepositoryPath=mvn-repo
+			echo "compile 'extra:${ARTIFACT}:${VERSION}'" >> "${GRADLE_DEPENDENCIES}"
+		fi
+	done
+done
+
+# end
