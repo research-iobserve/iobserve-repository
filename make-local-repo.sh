f642abc7471463410b5d51abaea9a6f96fca1fa6
Tobias Pöppke <t.poeppke@gmx.de>
2017-05-01 14:18:16 +0200

    Added pcm dependencies

--- make-local-repo.sh
+++ make-local-repo.sh
@@ -13,11 +13,18 @@ org.palladiosimulator.solver.context
 org.palladiosimulator.analyzer.workflow
 org.palladiosimulator.analyzer.completions
 org.palladiosimulator.analyzer.quality
+org.palladiosimulator.analyzer.resultdecorator
 de.uka.ipd.sdq.workflow.mdsd
 de.uka.ipd.sdq.workflow.launchconfig
 de.uka.ipd.sdq.workflow.logging.console
 de.uka.ipd.sdq.workflow.ui
+de.uka.ipd.sdq.workflow
 de.uka.ipd.sdq.units
+de.uka.ipd.sdq.statistics
+de.uka.ipd.sdq.pcm.stochasticexpressions
+de.uka.ipd.sdq.featuremodel
+de.uka.ipd.sdq.errorhandling
+de.uka.ipd.sdq.tcfmoop
 de.uka.ipd.sdq.stoex
 de.uka.ipd.sdq.stoex.analyse
 de.uka.ipd.sdq.probfunction
@@ -41,6 +48,14 @@ org.eclipse.ocl.ecore
 org.eclipse.net4j
 org.eclipse.net4j.util
 org.eclipse.equinox.common
+org.eclipse.xpand
+org.eclipse.xtend.core
+org.eclipse.xtend.typesystem.emf
+org.eclipse.xtend.standalone
+org.eclipse.xtend
+org.eclipse.lib
+org.eclipse.lib.macro
+
 lpg.runtime.java
 EOF`
 
@@ -61,6 +76,7 @@ for I in $LIST ; do
 	echo "Op $I"
 	for P in `find ${LIBS} -name "${I}_*.jar"` ; do
 		echo "$P"
+        S=`find ${LIBS} -name "${I}.source_*.jar" | head -1`
 		if [ -f "$P" ] ; then
 			N=`basename $P`
 			ARTIFACT=`echo $N | sed 's/^\(.*\)_.*$/\1/'`
@@ -70,7 +86,7 @@ for I in $LIST ; do
 		#	echo $VERSION
 			mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file -Dfile="$P" \
 				-DgroupId=extra -DartifactId=$ARTIFACT -Dpackaging=jar -Dversion=$VERSION \
-				-DlocalRepositoryPath=mvn-repo
+				-DlocalRepositoryPath=mvn-repo -Dsources="$S"
 			echo "compile 'extra:${ARTIFACT}:${VERSION}'" >> "${GRADLE_DEPENDENCIES}"
 		fi
 	done
